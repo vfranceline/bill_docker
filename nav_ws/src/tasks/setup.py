6d2a2e0d832f13fef3aab5618065d0a5594aa6b8
Vitória Matos <vitorianascimentomatos@gmail.com>
2025-06-16 18:45:44 -0300

    :bricks: feat: add goal_pose

---
+++ nav_ws/src/tasks/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'tasks'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='vfran',
+    maintainer_email='vitorianascimentomatos@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'goal_pose = tasks.goal_pose:main',
+        ],
+    },
+)
